7548ed177acce845f2841894895f3bcf6f0b1e46
Ivan Pashchenko <ivan.pashchenko@jetbrains.com>
2018-09-03 13:09:17 +0300

    1

--- build.sh
+++ build.sh
@@ -2,5 +2,7 @@
 set -e
 set -o pipefail
 
-pushd rider
-./gradlew --info --stacktrace buildPlugin
+curl -vL https://cache-redirector.jetbrains.com/maven-central/org/jetbrains/kotlin/kotlin-annotation-processing-gradle/1.2.41/kotlin-annotation-processing-gradle-1.2.41.pom
+
+#pushd rider
+#./gradlew --info --stacktrace buildPlugin
