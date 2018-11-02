d52a40e2cb2825349db2ce9f03f1592fabe23b57
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2018-11-02 17:01:50 +0100

    minor improvement

--- unity/EditorPlugin/Notifications.cs
+++ unity/EditorPlugin/Notifications.cs
@@ -11,9 +11,8 @@ namespace JetBrains.Rider.Unity.Editor
       if (RiderScriptableSingleton.Instance.AutoSaveWarningShownOnce || !PluginSettings.ShowAutoSaveWarning) return;
       
       var notification =
-        "AutoSaving files in Rider may trigger recompilation when switching back to Unity Editor, which may wipe live data if the Unity player is running." 
-        + Environment.NewLine +
-        "Consider changing \"Script Changes While Playing\" setting in Unity-Preferences-";
+       @"AutoSaving files in Rider may trigger recompilation when switching back to Unity Editor, which may wipe live data if the Unity player is running.
+       Consider changing <b>Script Changes While Playing</b> setting in Unity-Preferences-";
       if (UnityUtils.UnityVersion >= new Version(2018, 2) && EditorPrefsWrapper.ScriptChangesDuringPlayOptions == 0)
       {
         notification += "General";
